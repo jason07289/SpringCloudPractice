8da740da17a971cbed90cdfb762812bc2bce0fdf
jaehyuk0728 <jaehyuk0728@01-04396.nsseshop.com>
2022-02-03 17:16:41 +0900

    config client init

---
+++ SpringCloudEx-config-client/src/main/java/com/jaehyuk/springCloud/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.jaehyuk.springCloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jaehyuk.springCloud.service.DynamicConfigService;
+import com.jaehyuk.springCloud.service.StaticConfigService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ConfigController {
+	
+	private final StaticConfigService staticConfigService;
+	private final DynamicConfigService configDynamicService;
+	
+	@GetMapping(value = "/static")
+	public Object getConfigFromStatic() {
+		return staticConfigService.getConfig();
+	}
+
+	@GetMapping(value = "/dynamic")
+	public Object getConfigFromDynamic() {
+		return configDynamicService.getConfig();
+	}
+}
