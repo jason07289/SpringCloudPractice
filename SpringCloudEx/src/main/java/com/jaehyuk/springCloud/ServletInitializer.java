8e5a04d1bf603cd5a94ec6b9b45a5752f497e07c
jaehyuk0728 <jaehyuk0728@01-04396.nsseshop.com>
2022-02-03 14:05:29 +0900

    first commit

---
+++ SpringCloudEx/src/main/java/com/jaehyuk/springCloud/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jaehyuk.springCloud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCloudExApplication.class);
+	}
+
+}
---
+++ SpringCloudEx/src/main/java/com/jaehyuk/springCloud/SpringCloudExApplication.java
@@ -0,0 +1,13 @@
+package com.jaehyuk.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExApplication.class, args);
+	}
+
+}
